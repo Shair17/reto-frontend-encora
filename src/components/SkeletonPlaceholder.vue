41815f2e799308ee13c3e29d5393ee24b40d695f
Shair <jimmy_jhair17@hotmail.com>
2025-07-01 01:29:38 -0500

    ✨ feat: add `SkeletonPlaceholder` component

---
+++ src/components/SkeletonPlaceholder.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+import { cn } from '@/utils/cn';
+
+interface SkeletonProps {
+  class?: HTMLAttributes['class'];
+}
+
+const props = defineProps<SkeletonProps>();
+</script>
+
+<template>
+  <div :class="cn('animate-pulse rounded-md bg-neutral-200/80', props.class)" />
+</template>

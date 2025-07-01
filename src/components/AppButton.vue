9b5d3b85dd17831ce2c1d051a504958fe06fa9b7
Shair <jimmy_jhair17@hotmail.com>
2025-07-01 01:28:48 -0500

    ✨ feat: add `Button` component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { cn } from '@/utils/cn';
+import { cva } from 'class-variance-authority';
+
+const buttonVariants = cva(
+  "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        default: 'bg-reddit text-white shadow-xs hover:bg-reddit/80',
+        destructive:
+          'bg-red-500 text-white shadow-xs hover:bg-red-500/80 focus-visible:ring-red-500/20 dark:focus-visible:ring-red-500/40',
+        outline:
+          'border bg-background shadow-xs hover:bg-reddit hover:text-white dark:bg-reddit/30 dark:border-reddit dark:hover:bg-reddit/50',
+        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
+        ghost: 'hover:bg-reddit hover:text-white dark:hover:bg-reddit/50',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
+      size: {
+        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        icon: 'size-9',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
+interface Props {
+  variant?: NonNullable<Parameters<typeof buttonVariants>[0]>['variant'];
+  size?: NonNullable<Parameters<typeof buttonVariants>[0]>['size'];
+  as?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  as: 'button',
+});
+</script>
+
+<template>
+  <component :is="as" :class="cn(buttonVariants({ variant, size }), $attrs.class ?? '')">
+    <slot />
+  </component>
+</template>

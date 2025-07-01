5557ca11bc3987e8e6f5b34bbecf0c267bba2b15
Shair <jimmy_jhair17@hotmail.com>
2025-07-01 01:25:37 -0500

    ✨ feat: add icons

---
+++ src/components/icons/IconArrowUp.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+    :stroke="color"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    v-bind="$attrs"
+  >
+    <path d="m5 12 7-7 7 7" />
+    <path d="M12 19V5" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size?: string | number;
+    color?: string;
+  }>(),
+  {
+    size: '24',
+    color: 'currentColor',
+  },
+);
+</script>

05b4f28e26660da92a897e30cc75334b86d60a28
Shair <jimmy_jhair17@hotmail.com>
2025-07-01 01:21:36 -0500

    ✨ feat: add core components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { Toaster } from 'vue-sonner';
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 </script>
 
 <template>
+  <!-- View -->
   <RouterView />
+
+  <!-- Sonner Toast -->
+  <Toaster position="bottom-right" richColors />
+
+  <!-- VueQuery Devtools -->
+  <VueQueryDevtools button-position="bottom-left" />
 </template>
